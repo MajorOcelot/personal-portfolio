6dc4f21bee9ff44b06b4e3e22af762195b996157
MajorOcelot <161778806+MajorOcelot@users.noreply.github.com>
2025-06-02 01:27:26 -0500

    added some placeholder cards and added some javascript functionality and customization

--- js/script.js
+++ js/script.js
@@ -5,3 +5,28 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         target?.scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+const sections = document.querySelectorAll('.scroll-section');
+const navLinks = document.querySelectorAll('.nav-links a');
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            const link = document.querySelector(`.nav-links a[href="#${id}"]`);
+
+            if (entry.isIntersecting) {
+                navLinks.forEach(link => link.classList.remove('active'));
+                link?.classList.add('active');
+            }
+        });
+    },
+    {
+      root: null,
+      threshold: 0.5, // Trigger when at least 50% of section is visible
+    }
+  );
+
+    sections.forEach(section => {
+        observer.observe(section);
+});
